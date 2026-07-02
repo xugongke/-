5ded83995feae4715c3884d91687302b0482087a
徐攻克 <1528575294@qq.com>
2026-07-02 16:20:19 +0800

    准备添加设置备注楼栋号之前的代码

--- Solar_energy/Core/Src/main.c
+++ Solar_energy/Core/Src/main.c
@@ -24,6 +24,7 @@
 #include "dma.h"
 #include "fatfs.h"
 #include "i2c.h"
+#include "iwdg.h"
 #include "sdio.h"
 #include "spi.h"
 #include "tim.h"
@@ -121,6 +122,7 @@ int main(void)
   MX_ADC3_Init();
   MX_ADC1_Init();
   MX_CRC_Init();
+  MX_IWDG_Init();
   /* USER CODE BEGIN 2 */
 	Battery_Init();
 	RS485_USART_Init_All();
@@ -167,8 +169,9 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 15;
